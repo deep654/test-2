450ea7f192bd6c72d8127c202aef4c66dcfd9734
deep654 <Sudipta@#@1992>
2020-02-20 16:45:36 +0530

    commit

--- hi.py
+++ hi.py
@@ -1,4 +1,7 @@
-print("hiisaa")
-print("palash")
-
-print("1")
+i = int(input("enter no: "))
+if i >=10:
+    for j in range(10, i):
+        if j % 2 == 0:
+            print(j, "even no")
+        else:print(j, "odd no")
+else: print("not valid no")
